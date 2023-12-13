f132c8efc83221dd10820c50ba8bd05eebb81981
Xiaowen <jxw1102@gmail.com>
2023-12-14 02:19:17 +0800

    Update app.py
    Added Dev Container Folder
    
    model selection
    
    use 4 by default, 3.5 performs bad
    
    fix display order
    
    display upload error
    
    fix password

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from openai.types.beta.threads import MessageContentImageFile
 
 api_key = os.environ.get("OPENAI_API_KEY")
 auth_password = os.environ.get("AUTH_PASSWORD")
-assistant_id = os.environ.get("ASSISTANT_ID")
-instructions = os.environ.get("RUN_INSTRUCTIONS", "")
+assistant_id_35 = os.environ.get("ASSISTANT_ID_35")
+assistant_id_4 = os.environ.get("ASSISTANT_ID_4")
 
 # Initialize the OpenAI client (ensure to set your API key in the sidebar within the app)
 client = openai
@@ -32,6 +32,9 @@ if "message_ids" not in st.session_state:
 if "in_progress" not in st.session_state:
     st.session_state.in_progress = False
 
+if "assistant_id" not in st.session_state:
+    st.session_state.assistant_id = assistant_id_4
+
 # Set up the Streamlit page with a title and icon
 st.set_page_config(page_title="Data Analysis Assistant", page_icon=":speech_balloon:")
 
@@ -42,12 +45,23 @@ def upload_to_openai(uploaded_file):
     return response.id
 
 # Create a sidebar for API key configuration and additional features
+openai.api_key = None
 st.sidebar.header("Authentication")
 password = st.sidebar.text_input("Enter your password", type="password")
 if password and password == auth_password:
     openai.api_key = api_key
 elif len(password) > 0:
     st.sidebar.warning("Wrong password.")
+
+# Select model
+model = st.sidebar.selectbox(
+    "Select a model",
+    ("GPT-4 Turbo", "GPT-3.5 Turbo")
+)
+if model == "GPT-4 Turbo":
+    st.session_state.assistant_id = assistant_id_4
+else:
+    st.session_state.assistant_id = assistant_id_35
 
 # Additional features in the sidebar for web scraping and file uploading
 st.sidebar.header("Files")
@@ -59,9 +73,12 @@ uploaded_file = st.sidebar.file_uploader("Upload a file to OpenAI", key="file_up
 if st.sidebar.button("Upload File"):
     # Upload file provided by user
     if uploaded_file:
-        additional_file_id = upload_to_openai(uploaded_file)
-        st.session_state.file_id_list.append(additional_file_id)
-        st.sidebar.write(f"File ID: {additional_file_id}")
+        try:
+            additional_file_id = upload_to_openai(uploaded_file)
+            st.session_state.file_id_list.append(additional_file_id)
+            st.sidebar.write(f"File ID: {additional_file_id}")
+        except openai.BadRequestError as e:
+            st.sidebar.write(str(e))
 
 # Display all file IDs
 if st.session_state.file_id_list:
@@ -70,7 +87,7 @@ if st.session_state.file_id_list:
         st.sidebar.write(file_id)
         # # Associate files with the assistant
         # assistant_file = client.beta.assistants.files.create(
-        #     assistant_id=assistant_id, 
+        #     assistant_id=st.session_state.assistant_id, 
         #     file_id=file_id
         # )
 
@@ -165,7 +182,7 @@ if st.session_state.start_chat:
         # Create a run with additional instructions
         run = client.beta.threads.runs.create(
             thread_id=st.session_state.thread_id,
-            assistant_id=assistant_id,
+            assistant_id=st.session_state.assistant_id,
             instructions="Please answer the queries using the data provided in the files. When adding other information mark it clearly as such, with different color"
         )
 
@@ -180,7 +197,7 @@ if st.session_state.start_chat:
                 message for message in messages 
                 if message.role == "assistant"
             ]
-            for message in assistant_messages_for_run:
+            for message in reversed(assistant_messages_for_run):
                 if message.id in st.session_state.message_ids:
                     continue
                 full_text, images = process_message_with_citations(message)
